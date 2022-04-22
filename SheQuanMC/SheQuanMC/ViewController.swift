ebdddc2781c5fc59628a98c2af0286c56a7424cc
419425903@qq.com <419425903@qq.com>
2022-04-22 17:48:56 +0800

    添加了一些本地库

--- SheQuanMC/SheQuanMC/ViewController.swift
+++ SheQuanMC/SheQuanMC/ViewController.swift
@@ -6,13 +6,20 @@
 //
 
 import UIKit
+import LDStreaming
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let reuqest = LDStreamingRequest()
+        LDClient.request(reuqest)
+        
     }
+    
 
 
 }
